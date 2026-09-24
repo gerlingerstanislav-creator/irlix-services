a53745da2311c1a49372784119a77c348a9aa596
gerlingerstanislav-creator <gerlinger.stanislav@gmail.com>
2026-09-24 21:35:39 +0400

    feat(ui): export shared design system primitives

---
+++ packages/ui/src/index.js
@@ -0,0 +1,4 @@
+export { default as UiBadge } from './components/UiBadge.vue';
+export { default as UiButton } from './components/UiButton.vue';
+export { default as UiPageHeader } from './components/UiPageHeader.vue';
+export { default as UiPanel } from './components/UiPanel.vue';
